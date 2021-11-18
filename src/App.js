37c9dc82771f98e747852a35da8bab047da3eb3b
phoebe-q <phoebe.quinn04@gmail.com>
2021-11-18 17:06:22 +0000

    scanner component now working

--- src/App.js
+++ src/App.js
@@ -1,78 +1,16 @@
-import React, { useState, useRef, useEffect, useContext, Component, useLayoutEffect } from "react";
-import Popup from "reactjs-popup";
-import { GoogleMap, Wrapper, InfoWindow, Marker, useLoadScript } from "@react-google-maps/api";
+import React, { Component } from 'react';
+import './App.css';
+import HomePage from "./pages/HomePage"
 
-import BarcodePopup from './BarcodePopup';
 
-require("./BarcodePopup.css");
-
-const glasgow = { lat: 55.8642, lng: -4.2518 };
-
-const mapContainerStyle = {
-    width: '100wv',
-    height: '100vh',
-};
-
-// let user_location = {lat: 0, lng: 0}; 
-let user_location = glasgow
-
-export default function App() {
-    // !!! currently broken, will implement later !!!
-    // async function getUserLocation() {
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(
-    //           (position) => {
-    //             const pos = {
-    //               lat: position.coords.latitude,
-    //               lng: position.coords.longitude,
-    //             };
-    //             user_location = pos
-    //           }
-    //         );
-    //       } else {
-    //         // Browser doesn't support Geolocation
-    //         user_location = {glasgow}
-    //     }
-    //     return user_location
-    // }
-
-    // await getUserLocation();
-
-    const {isLoaded, loadError} = useLoadScript({
-        googleMapsApiKey: 'AIzaSyAYtDG4yYGDrPP5zZF2OqYBwQ4pA8-w6Wk',
-    })
-
-    if (loadError) return "Error loading maps";
-    if (!isLoaded) return "Loading maps";
-
+class App extends Component {
+  render() {
     return (
-        <div>
-            <GoogleMap 
-            mapContainerStyle={mapContainerStyle}
-            zoom={4}
-            center={user_location}>
-                <Marker
-                id = "user_pos"
-                position = {user_location}
-                >    
-                </Marker>
-                <Popup
-                    trigger={() => (
-                        <button
-                        className="barcode"
-                        >
-                        Scan Item
-                        </button>
-                    )}
-                    position="top left"
-                    closeOnDocumentClick
-                    >
-                    <span>
-                        <BarcodePopup></BarcodePopup>
-                    </span>
-                </Popup>
-                
-            </GoogleMap>
-        </div>
+      <div className="App">
+        <HomePage/>
+      </div>
     );
+  }
 }
+
+export default App;
