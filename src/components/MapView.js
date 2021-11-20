bf62bed65e290e1ebacf74bc136c72b3b08c6936
Alex <2382854p@student.gla.ac.uk>
2021-11-20 19:59:15 +0000

    checkpoint

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect, useContext, Component, useLayoutEffect } from "react";
 import Popup from "reactjs-popup";
-import { GoogleMap, Wrapper, InfoWindow, Marker, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, Marker, useLoadScript, Polyline } from "@react-google-maps/api";
 
 import BarcodePopup from './BarcodePopup';
 
@@ -17,6 +17,36 @@ const mapContainerStyle = {
 let user_location = glasgow
 
 export default function MapView() {
+    const [icons, setIcons] = useState(
+        [
+            { icon: 
+                {
+                    path: window.google ? window.google.maps.SymbolPath.FORWARD_CLOSED_ARROW : null, 
+                    scale: 8, 
+                    strokeColor: "#A6B7D7"
+                }, 
+                offset: "100%" 
+            }
+        ]
+        );
+    const [path, setPath] = useState([
+        { lat: 56, lng: -5.3518 },
+        { lat: 56.8642, lng: -5.3518 },
+    ]);
+    const mapRef = useRef()
+    const polylineRef = useRef(Polyline)
+    const [line, setLine] = useState(polylineRef)
+
+    useLayoutEffect(() => {
+        // console.log(window.google)
+        if (window.google) {
+            icons[0].icon.path = window.google.maps.SymbolPath.BACKWARD_CLOSED_ARROW;
+            // setIcons(icons);
+            animateCircle(line)
+        }
+    });
+    
+
     // !!! currently broken, will implement later !!!
     // async function getUserLocation() {
     //     if (navigator.geolocation) {
@@ -45,9 +75,40 @@ export default function MapView() {
     if (loadError) return "Error loading maps";
     if (!isLoaded) return "Loading maps";
 
+    // const path = [
+    //     { lat: 56, lng: -5.3518 },
+    //     { lat: 56.8642, lng: -5.3518 },
+    // ]
+
+    // Use the DOM setInterval() function to change the offset of the symbol
+// at fixed intervals.
+
+    // state is changing but not reflected in frontend
+    function animateCircle(line) {
+        console.log("function again")
+        let count = 0;
+        console.log(line);
+            window.setInterval(() => {
+                count = (count + 1) % 200;
+                let x = line.current.props.icons;
+                let y = line.current.props.path;
+                console.log(icons)
+                x[0].offset = count / 2 + "%";
+                console.log(icons)
+                y[0].lng = y[0].lng+1;
+                // setIcons(x);
+                // setPath(y);
+                setLine(x);
+            }, 5000);
+        
+    }
+
+    console.log(icons);
+    console.log(path);
     return (
         <div>
-            <GoogleMap 
+            <GoogleMap
+            ref = {mapRef} 
             mapContainerStyle={mapContainerStyle}
             zoom={4}
             center={user_location}>
@@ -56,6 +117,12 @@ export default function MapView() {
                 position = {user_location}
                 >    
                 </Marker>
+                <Polyline
+                    ref={polylineRef}
+                    path={path}
+                    icons={icons}
+                    map={mapRef}
+                />  
                 <Popup
                     trigger={() => (
                         <button
@@ -76,3 +143,4 @@ export default function MapView() {
         </div>
     );
 }
+
