a597f977a43a09b9b4838a204ae5fd25da97e72c
OoKaioO <kailin317@gmail.com>
2021-11-28 13:37:32 +0000

    Changed the leaderboard so it is consistent with the newest design

--- src/components/LeaderBoardView.js
+++ src/components/LeaderBoardView.js
@@ -1,108 +1,114 @@
 import React, {Component} from 'react'
 import {getScore} from "../components/MapView"
 
-class LeaderBoardView extends Component {
-    constructor(){
-        super()
+export default function LeaderBoardView(props) {
 
-        
-        this.state = {
             //Dictionary stores the different player and scores
             // finalScore: MapView.getScore(),
-            users:[
-                {
-                    name:  'John',
-                    score: "20"
-                
-                },
-                {
-                    name:  'John',
-                    score: "10"
-                
-                },
-                {
-                    name:  'John',
-                    score: "40"
-                
-                },
-                {
-                    name:  'John',
-                    score: "40"
-                
-                },
-                {
-                    name:  'John',
-                    score: "30"
-                
-                },
-                {
-                    name:  'John',
-                    score: "30"
-                
-                },
-            ],
-            
+        var ScottishCities = [
+                    {
+                        name:  'Inverness',
+                    },
+                    {
+                        name:  'Edingburgh',
+                    
+                    },
+                    {
+                        name:  'Glasgow',
+                    
+                    },
+                    {
+                        name:  'Dundee',
 
-
-
-
-
+                    
+                    },
+                    {
+                        name:  'Aberdeen',
+   
+                    
+                    },
+            ]
             
             
-        }
-       
-
-        this.getRanking = this.getRanking.bind(this);
-
-
-
-        // console.log(typeof(this.state.users));
-    }
-
-
-
-    getRanking(){
-        var sortthis = this.state.users;
-        for (var i in sortthis){
-            console.log(sortthis[i]["score"]);
-            console.log(typeof(sortthis))
-        }
-        console.log("score = ", this.state.finalScore)
-
-        var sorted = sortthis.sort((a,b) => (a.score>b.score) ? 1: - 1)
         
-        return sorted;
 
+
+        var UkCities = [
+            {
+                name:  'Manchester',
+            },
+            {
+                name:  'Cornwall',
+            
+            },
+            {
+                name:  'Warreck',
+            
+            },
+            {
+                name:  'Inverness',
+            
+            },
+            {
+                name:  'Brighton',
+
+            
+            },
+
+    ]
+            
+            
+            
+            return(
+                <div className = "Block">
+                    <table className="Score Table">
+                        <tbody className = "Scottish">
+                            {       
+                                ScottishCities.map((user, rank) => {
+                                    return (
+
+                                        <tr >
+                                            <td>{rank+1}</td>
+                                            <td>{user.name}</td>
+                                        </tr>
+
+                                    )
+                                })
+    
+                            }
+                            
+                        </tbody>
+                        <tbody className = "UK">
+                            {       
+                                UkCities.map((user, rank) => {
+                                    return (
+
+                                        <tr >
+                                            <td>{rank+1}</td>
+                                            <td>{user.name}</td>
+                                        </tr>
+
+                                    )
+                                })
+    
+                            }
+                            
+                        </tbody>
+                    </table>
+                </div>
+            )
+            
+        
     }
 
+
+
+    
     
 
     
 
 
 
-    render(){
-        return(
-            <div className = "Block">
-                <table className="Score Table">
-                    <tbody>
-                        {
-                            this.getRanking().map((user, rank) => {
-                                return (
-                                    <tr >
-                                        <td>{rank+1}</td>
-                                        <td>{user.name}</td>
-                                        <td><b>{user.score}</b></td> 
-                                    </tr>
-                                )
-                            })
-
-                        }
-                        
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-} 
-export default LeaderBoardView;
+    
+ 
