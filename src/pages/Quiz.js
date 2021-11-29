1d27e3d80d5bb5a3cf8970a532f75f2ea95614f6
phoebe-q <phoebe.quinn04@gmail.com>
2021-11-29 13:28:09 +0000

    made css fixes to match design

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -2,15 +2,10 @@ import React from 'react'
 
 import Question from "../components/Question"
 
-const checkAnswer = () => {
-    console.log("You clicked me")
-}
-
 export default function Quiz() {
     return (
         <div>
-            <h1>Fly Your Food</h1>
-            <Question></Question>
+            <Question />
         </div>
     )
 }
